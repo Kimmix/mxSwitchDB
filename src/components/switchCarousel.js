2791978cc86274787a500606bfd149021750ac15
Kimmix <kimmix05@gmail.com>
2020-07-05 18:17:32 +0700

    add debug mode

--- src/components/switchCarousel.js
+++ src/components/switchCarousel.js
@@ -4,25 +4,44 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import switches from "../resource/switchDB.json"
-const switchData = switches;
+
 
 export default class SwitchCarousel extends Component {
   constructor(props) {
     super(props);
-    this.state = { isDebug: false };
+    this.state = {
+      isDebug: false,
+      search: '',
+      switchData: switches
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      search: e.target.value,
+      switchData: switches.filter(s => s.name.includes(e.target.value))
+    });
   }
 
   render() {
-    const { isDebug } = this.state;
-    const settings = {
+    const { isDebug, switchData } = this.state;
+    var settings = {
+      infinite: true,
       speed: 500,
-      infinite: false,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      lazyLoad: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
       responsive: [
         {
-          breakpoint: 600,
+          breakpoint: 960,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          }
+        },
+        {
+          breakpoint: 720,
           settings: {
             slidesToShow: 2,
             slidesToScroll: 2,
@@ -39,6 +58,15 @@ export default class SwitchCarousel extends Component {
     };
     let mode;
     if (isDebug) {
+      mode = <div>
+        <h3>Debuging</h3>
+        {switchData.map((data) => (
+          <ul key={data.id}>
+            <li>{data.name}</li>
+          </ul>
+        ))}
+      </div>
+    } else {
       mode = <Slider {...settings}>
         {switchData.map((data) => (
           <div key={data.id}>
@@ -46,21 +74,21 @@ export default class SwitchCarousel extends Component {
           </div>
         ))}
       </Slider>
-    } else {
-      mode = <div>
-        <h1>Debugging</h1>
-        {switchData.map((data) => (
-          <ul key={data.id}>
-            <li>{data.name}</li>
-          </ul>
-        ))}
-      </div>
     }
+
+
     return (
-      <div className="container">
-        <p>{this.state.isDebug}</p>
-        <button type="button" onClick={() => this.setState({ isDebug: !this.state.isDebug })}>Debug mode</button>
-        {mode}
+      <div>
+        <div class="row">
+          <div class="six columns">
+            <input class="u-full-width" type="text" placeholder="Search" value={this.state.search} onChange={(e) => { this.handleChange(e) }} />
+          </div>
+        </div>
+        <div class="row">
+          <button type="button" className={`${this.state.isDebug ? "button-primary" : ""}`}
+            onClick={() => this.setState({ isDebug: !this.state.isDebug })}>Debug mode</button>
+          {mode}
+        </div>
       </div>
     );
   }
