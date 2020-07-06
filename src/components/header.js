c88f20a6ec0867dc835f5626588d89c635d568eb
Kimmix <kimmix05@gmail.com>
2020-07-06 17:30:14 +0700

    search func

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,31 @@
+import React, { useState } from "react"
 import { Link } from "gatsby"
-import React from "react"
 
-const Header = () => (
-  <header>
-    <Link
-      to="/"
-      style={{
-        fontSize: "2.5rem",
-        textDecoration: "none",
-        color: "black",
-        fontWeight: "bold",
-      }}
-    >
-      MX Switch DB
-    </Link>
-    <input type="text" name="search" style={{ margin: "1em 0" }} />
-  </header>
-)
+function Header() {
+  const [search, setSearch] = useState("")
+
+  return (
+    <header>
+      <Link
+        to="/"
+        style={{
+          fontSize: "2.5rem",
+          textDecoration: "none",
+          color: "black",
+          fontWeight: "bold",
+        }}
+      >
+        MX Switch DB
+      </Link>
+      <input
+        type="text"
+        style={{ margin: "1em 0" }}
+        placeholder="Search"
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+      />
+    </header>
+  )
+}
 
 export default Header
